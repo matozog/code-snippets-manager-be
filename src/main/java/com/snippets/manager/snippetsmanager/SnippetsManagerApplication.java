a45fe57be982478a8322dea25b733b2bdb9df7d6
Mateusz Ożóg <ozog.mat@o2.pl>
2022-12-03 12:15:20 +0100

    Generate project

---
+++ src/main/java/com/snippets/manager/snippetsmanager/SnippetsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.snippets.manager.snippetsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snippets/manager/snippetsmanager/SnippetsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snippets.manager.snippetsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
